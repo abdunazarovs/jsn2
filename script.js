b933365fa1da47b4926e15afe45a636dacf69f29
unknown <abu040810@gmail.com>
2025-02-07 16:10:18 +0500

    prompt done

--- script.js
+++ script.js
@@ -3,5 +3,9 @@
 //const message=alert('HELLO WORLD');
 //console.log(message);
 // confirm inglizchadan tasdiqlash va ikki xil malumot qaytaradi yoki true yoki false 
-const age=confirm("are you above 18?");
-console.log(age);
+//const age=confirm("are you above 18?");
+//console.log(age);
+
+const message=prompt("ismingizni kiriting!");
+console.log(typeof(message));//null 
+// typeof ->malumot turini bilishchun
