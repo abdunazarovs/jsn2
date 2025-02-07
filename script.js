55779f1c65d2ce4477eed0932814b8d69a9c692a
unknown <abu040810@gmail.com>
2025-02-07 16:23:47 +0500

    prompt done

--- script.js
+++ script.js
@@ -6,6 +6,22 @@
 //const age=confirm("are you above 18?");
 //console.log(age);
 
-const message=prompt("ismingizni kiriting!");
-console.log(typeof(message));//null 
-// typeof ->malumot turini bilishchun
+//const message=+prompt(" son kiriting");
+//console.log(typeof(message));//null 
+// typeof ->malumot turini bilishchun4
+
+
+// string nima?
+// char 
+// string bu so'zlar bilan ishlash uchun ishlatamiz  u chardan farqi xohlasez 1ta harfdi yozin xohlasez butun gapdi
+
+let a='Hello';
+//a=Number(a);
+let b=prompt("ikkinCHI SON");
+//b=Number(b);
+let c=a-b;
+// 1usul prompt sozini oldiga +
+// 2usul prompt ovolas paski qatorga Number funksiyasini yozb olasiz
+// minusda string ham amal qilyapti lekin plus ishlamayapti
+//minusda ozgaruvchidan bittasi string bop qosa NaN qaytaradi (Raqam emas)
+console.log(c);
